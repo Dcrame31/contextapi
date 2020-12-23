a40a79184cda3826fe311a8b8b0fa0030fc68eb9
Diane Crame <dcrame@icloud.com>
2020-12-23 14:37:31 -0800

    add styling to App class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import ToDoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
       <div className="ui raised very padded text container segment">
         <Navbar/>
+        <ToDoList/>
       </div>
     </div>
   );
