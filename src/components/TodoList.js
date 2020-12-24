04bc1dd252be79e3d609d63dfc6f0e7fbd81ce73
Diane Crame <dcrame@icloud.com>
2020-12-23 20:54:27 -0800

    add ThemeContext to ToDoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class ToDoList extends React.Component {
+    static contextType = ThemeContext;
+
     render() {
+        const { isDarkTheme, lightTheme, darkTheme} = this.context;
+        const theme = isDarkTheme ? darkTheme : lightTheme;
         return (
-            <div className="ui list">
+            <div style={{ background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}}>
                 <p className="item"> Plan the family trip</p>
                 <p className="item">Go shopping for plants</p>
                 <p className="item">Go on a hike</p>
