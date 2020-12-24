f85837a0790324801c24f5865127eb7a37b89729
Diane Crame <dcrame@icloud.com>
2020-12-23 20:19:01 -0800

    add ThemeContextProvider

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,25 @@
+import React, {createContext} from 'react';
+
+export const ThemeContext = createContext();
+
+class ThemeContextProvider extends React.Component {
+    state = {
+        isDarkTheme: true,
+        lightTheme: {
+            text: '#222',
+            background: '#d8ddf1'
+        },
+        darkTheme: {
+            text: "#fff",
+            background: '#5c5c5c'
+        }
+    }
+
+    render() {
+        return (
+         
+        )
+    }
+ }
+
+ export default ThemeContextProvider;
