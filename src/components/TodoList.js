de08fb5552c29abf2e18664877b194c39a5bf047
Diane Crame <dcrame@icloud.com>
2020-12-29 14:50:48 -0800

    add useContext hook

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { AuthContext} from '../contexts/AuthContext';
 
-class ToDoList extends React.Component {
+// class ToDoList extends React.Component {
   
 
-    render() {
-        // const { isDarkTheme, lightTheme, darkTheme, changeTheme} = this.context;
-        // const theme = isDarkTheme ? darkTheme : lightTheme;
-        return (
-            <ThemeContext.Consumer>{(context) => {
-                const { isDarkTheme, lightTheme, darkTheme, changeTheme} = context;
-                const theme = isDarkTheme ? darkTheme : lightTheme;
-                return (
+//     render() {
+//         // const { isDarkTheme, lightTheme, darkTheme, changeTheme} = this.context;
+//         // const theme = isDarkTheme ? darkTheme : lightTheme;
+//         return (
+//             <ThemeContext.Consumer>{(context) => {
+//                 const { isDarkTheme, lightTheme, darkTheme, changeTheme} = context;
+//                 const theme = isDarkTheme ? darkTheme : lightTheme;
+//                 return (
                     
-                    <div style={{ background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}}>
-                        <p className="item"> Plan the family trip</p>
-                        <p className="item">Go shopping for plants</p>
-                        <p className="item">Go on a hike</p>
-                        <button className="ui button primary" onClick={changeTheme}>Change Theme</button>
-                    </div>
-                        )
-            }}
-                </ThemeContext.Consumer>
-        )
-    }
+//                     <div style={{ background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}}>
+//                         <p className="item"> Plan the family trip</p>
+//                         <p className="item">Go shopping for plants</p>
+//                         <p className="item">Go on a hike</p>
+//                         <button className="ui button primary" onClick={changeTheme}>Change Theme</button>
+//                     </div>
+//                         )
+//             }}
+//                 </ThemeContext.Consumer>
+//         )
+//     }
+// }
+
+const TodoList = () => {
+    const { isDarkTheme, lightTheme, darkTheme, changeTheme} = useContext(ThemeContext);
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+    return (        
+            <div style={{ background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}}>
+                <p className="item"> Plan the family trip</p>
+                <p className="item">Go shopping for plants</p>
+                <p className="item">Go on a hike</p>
+                <button className="ui button primary" onClick={changeTheme}>Change Theme</button>
+            </div>
+    )
 }
 
-export default ToDoList;
+export default TodoList;
